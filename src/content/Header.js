661e95acbdc0ebddaca4adec95a63967149f206d
Mikael Motin <motin@chalmers.se>
2023-02-05 17:14:58 +0100

    added the divider of "="'s

--- src/content/Header.js
+++ src/content/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="p-6 bg-mid_darkblue">
+    <header className="py-4 bg-mid_darkblue">
       <div className="flex justify-end space-x-6">
         <a className="link"> #about </a>
         <a className="link" > #careers </a>
         <a className="link" > #contact </a>
         <a className="link italic" > Go To Gallery </a>
       </div>
+      <p className="py-1 overflow-hidden max-w-full whitespace-nowrap opacity-90 text-white"> =============================================================================================================================================================================================================================================================== </p>
     </header>
   )
 }
